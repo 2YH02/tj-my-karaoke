30164b813dc24c77760d133af7261eee76784e0c
2YH02 <yonghuni484@gmail.com>
2024-07-10 20:22:28 +0900

    프로젝트 환경 세팅

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      jsxImportSource: "@emotion/react",
+      babel: {
+        plugins: ["@emotion/babel-plugin"],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+      "@common": path.resolve(__dirname, "src/components/common"),
+      "@contexts": path.resolve(__dirname, "src/contexts"),
+    },
+  },
+});
